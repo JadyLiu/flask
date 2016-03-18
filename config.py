4b3e46598a9d3b2ac6e0bc31e923b7a7fc7e76f0
Jady Liu <jady.liu@alluremedia.com.au>
2016-03-18 15:00:52 +1100

    update config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = 'my precious'
+    SECRET_KEY = '2e~\x97\x99\x0f\x88 \x83\xab]\xc4O\x8d\xefAq\xb1\x8bGvh\x97\x08'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(BaseConfig):
