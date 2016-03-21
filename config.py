abcade04315343eeaca59b3ebf71abd6da76a3c3
Jady Liu <jady.liu@alluremedia.com.au>
2016-03-21 13:08:30 +1100

    added migrations

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
 # default config
+# export DATABASE_URL="postgresql://localhost/jd_flask"
+# export ENV_DB_CONNECTION_DSN = postgresql+psycopg2:///jd_flask
+# heroku config:set APP_SETTINGS=config.ProductionConfig --remote heroku
+# export DATABASE_URL="postgres://postgres:mysqlpass@localhost:5432/jd_flask"
 class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = '2e~\x97\x99\x0f\x88 \x83\xab]\xc4O\x8d\xefAq\xb1\x8bGvh\x97\x08'
--- db_create.py
+++ db_create.py
@@ -7,6 +7,7 @@ db.create_all()
 # insert 
 db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Well", "I\'m well."))
+db.session.add(BlogPost("postgrs", "we setup a local postgres instances"))
 
 # commit the changes
 db.session.commit()
